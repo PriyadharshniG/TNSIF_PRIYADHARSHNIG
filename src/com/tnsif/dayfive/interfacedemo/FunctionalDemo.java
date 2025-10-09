f137782b69b7642b98e799443a3ead59bd688c65
Priya <priyadharshni823@gmail.com>
2025-10-09 09:52:28 +0530

    completed day-five and day-6

---
+++ src/com/tnsif/dayfive/interfacedemo/FunctionalDemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.dayfive.interfacedemo;
+
+public class FunctionalDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Lambda Expression
+				GreetingDemo gd = () -> 
+				System.out.println("Hello Everyone, Welcome to the Java Session");
+				
+				gd.greet();
+
+
+	}
+
+}
---
+++ src/com/tnsif/daysix/exceptionhandling/WithoutExceptionHandling.java
@@ -0,0 +1,11 @@
+package com.tnsif.daysix.exceptionhandling;
+
+public class WithoutExceptionHandling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("the program continues....");
+		int data=100/0;
+	}
+
+}
